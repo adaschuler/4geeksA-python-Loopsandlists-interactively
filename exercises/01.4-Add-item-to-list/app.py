811ef81aa8f245a2d3ff5c4d3b0cd30c2c3635b8
adaschuler <adaschuler@gmail.com>
2021-07-19 21:53:23 +0000

    Python 2.1/16

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,18 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+def generate_random():
+    number = random.randint(0, 10)
+    return number
+
+print(generate_random())
+
+for x in range(10):
+    my_list.append(generate_random())
+
+    
+
+print(my_list)
--- exercises/02-Loop-list/app.py
+++ exercises/02-Loop-list/app.py
@@ -1,4 +1,13 @@
 my_list = [232,32,1,4,55,4,3,32,3,24,5,5,5,34,2,35,5365743,52,34,3,55]
 
 #Your code go here:
-print(my_list[0])
+
+""" i=0
+while i < len(my_list):
+    print(my_list[i])
+    i=i+1 """
+#lo de arriba tambien funciona, pero el test unitario pide hacerlo con for
+    
+[print(x) for x in my_list]
+
+
